d6fc6062b2814a770de345b4273065f150405900
bekkblando <bekkblando@gmail.com>
2019-06-04 15:44:24 -0400

    I was just messin' around with the bash process

---
+++ play_spotify.py
@@ -0,0 +1,110 @@
+"""
+Example on how to use the Spotify Controller.
+NOTE: You need to install the spotipy and spotify-token dependencies.
+
+This can be done by running the following:
+pip install spotify-token
+pip install git+https://github.com/plamere/spotipy.git
+"""
+import http.client as http_client
+import logging
+import time
+import sys
+
+import pychromecast
+from pychromecast.controllers.spotify import SpotifyController
+import spotify_token as st
+import spotipy
+
+CAST_NAME = "Sadie's TV"
+
+
+class ConnListener:
+    def __init__(self, mz):
+        self._mz=mz
+
+    def new_connection_status(self, connection_status):
+        """Handle reception of a new ConnectionStatus."""
+        if connection_status.status == 'CONNECTED':
+            self._mz.update_members()
+
+class MzListener:
+    def __init__(self):
+        self.got_members=False
+    
+    def multizone_member_added(self, uuid):
+        pass
+
+    def multizone_member_removed(self, uuid):
+        pass
+
+    def multizone_status_received(self):
+        self.got_members=True
+
+def play_spotify(user, password, uri = ["spotify:track:3Zwu2K0Qa5sT6teCCHPShP"], show_debug = False, cast = CAST_NAME):
+    if show_debug:
+        logging.basicConfig(level=logging.DEBUG)
+        # Uncomment to enable http.client debug log
+        #http_client.HTTPConnection.debuglevel = 1
+
+    chromecasts = pychromecast.get_chromecasts()
+    cast = None
+    for _cast in chromecasts:
+        if _cast.name == cast:
+            cast = _cast
+            break
+
+    if not cast:
+        print('No chromecast with name "{}" discovered'.format(cast))
+        print('Discovered casts: {}'.format(chromecasts))
+        sys.exit(1)
+
+    print('cast {}'.format(cast))
+
+    # Wait for connection to the chromecast
+    cast.wait()
+
+    spotify_device_id = None
+
+    # Create a spotify token
+    data = st.start_session(user, password)
+    access_token = data[0]
+    expires = data[1] - int(time.time())
+
+    # Create a spotify client
+    client = spotipy.Spotify(auth=access_token)
+    if show_debug:
+        spotipy.trace = True
+        spotipy.trace_out = True
+
+    # Launch the spotify app on the cast we want to cast to
+    sp = SpotifyController(access_token, expires)
+    cast.register_handler(sp)
+    sp.launch_app()
+
+    if not sp.is_launched and not sp.credential_error:
+        print('Failed to launch spotify controller due to timeout')
+        sys.exit(1)
+    if not sp.is_launched and sp.credential_error:
+        print('Failed to launch spotify controller due to credential error')
+        sys.exit(1)
+
+    # Query spotify for active devices
+    devices_available = client.devices()
+
+    # Match active spotify devices with the spotify controller's device id
+    for device in devices_available['devices']:
+        if device['id'] == sp.device:
+            spotify_device_id = device['id']
+            break
+
+    if not spotify_device_id:
+        print('No device with id "{}" known by Spotify'.format(sp.device))
+        print('Known devices: {}'.format(devices_available['devices']))
+        sys.exit(1)
+
+    # Start playback
+    if uri[0].find('track') > 0:
+        client.start_playback(device_id=spotify_device_id, uris=uri)
+    else:
+        client.start_playback(device_id=spotify_device_id, context_uri=uri[0])
--- server.py
+++ server.py
@@ -1,35 +1,17 @@
 from flask import Flask, request, redirect
 import subprocess
 import os
+from play_spotify import play_spotify
 
 # spotify:track:2Iq6HhIquO7JKr0KfTNLzU
 
 app = Flask(__name__)
 
-def run(*popenargs, input=None, check=False, **kwargs):
-    if input is not None:
-        if 'stdin' in kwargs:
-            raise ValueError('stdin and input arguments may not both be used.')
-        kwargs['stdin'] = subprocess.PIPE
-
-    process = subprocess.Popen(*popenargs, **kwargs)
-    try:
-        stdout, stderr = process.communicate(input)
-    except:
-        process.kill()
-        process.wait()
-        raise
-    retcode = process.poll()
-    if check and retcode:
-        raise subprocess.CalledProcessError(
-            retcode, process.args, output=stdout, stderr=stderr)
-    return retcode, stdout, stderr
-
 @app.route('/music')
 def music():
     spotify_uri = request.args.get('uri')
     # Play the music
-    print(run(["nohup", "python3", "/home/pi/Documents/QRCodeMusic/spotify_example.py", "--user", "Rafael09ED", "--password", os.environ['SPOTIFY_PASS'], "--uri", spotify_uri, "&"]))
+    play_spotify("Rafael09ED", os.environ['SPOTIFY_PASS'], spotify_uri, False, "Sadie's TV", )
     spotify, media_type, id = spotify_uri.split(":")
     return redirect("https://open.spotify.com/{media_type}/{id}".format(media_type=media_type, id = id), code=302)
 
