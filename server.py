fbe5b146748f301a6a84c11bb2b6a6db44bf0965
bekkblando <bekkblando@gmail.com>
2019-06-04 00:18:18 -0400

    a mix of terrible and amusing

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import subprocess
+import os
 
 app = Flask(__name__)
 
@@ -6,6 +8,7 @@ app = Flask(__name__)
 def play():
     spotify_uri = request.args.get('uri')
     # Play the music
+    subprocess.run(["python3", "/home/pi/Documents/QRCodeMusic/spotify_example.py", "--user", "rafeal09ED", "--password", os.environ['SPOTIFY_PASS']])
     return redirect('google.com', code=302)
 
 if __name__ == '__main__':
