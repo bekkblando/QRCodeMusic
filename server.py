62a19efa2fea7c34c8ccb8e8e997281f4a31a1a9
bekkblando <bekkblando@gmail.com>
2019-06-04 00:29:34 -0400

    fixed order

--- server.py
+++ server.py
@@ -5,17 +5,6 @@ import os
 
 app = Flask(__name__)
 
-@app.route('/play')
-def play():
-    spotify_uri = request.args.get('uri')
-    # Play the music
-    run(["python3", "/home/pi/Documents/QRCodeMusic/spotify_example.py", "--user", "rafeal09ED", "--password", os.environ['SPOTIFY_PASS']])
-    return redirect('google.com', code=302)
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
-
-
 def run(*popenargs, input=None, check=False, **kwargs):
     if input is not None:
         if 'stdin' in kwargs:
@@ -34,3 +23,13 @@ def run(*popenargs, input=None, check=False, **kwargs):
         raise subprocess.CalledProcessError(
             retcode, process.args, output=stdout, stderr=stderr)
     return retcode, stdout, stderr
+
+@app.route('/play')
+def play():
+    spotify_uri = request.args.get('uri')
+    # Play the music
+    run(["python3", "/home/pi/Documents/QRCodeMusic/spotify_example.py", "--user", "rafeal09ED", "--password", os.environ['SPOTIFY_PASS']])
+    return redirect('google.com', code=302)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
